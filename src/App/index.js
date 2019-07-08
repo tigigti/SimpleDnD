5d07889718e98b8ac3121c0cbcfe52071bab1a71
Angelos Ioannou <angelos.ioannou@gmx.de>
2019-07-09 01:06:51 +0200

    Added Router

--- src/App/index.js
+++ src/App/index.js
@@ -1,53 +1,12 @@
 import React from "react";
-import styled, { keyframes } from "styled-components";
-import { Card, Button } from "antd";
-
-const blendIn = keyframes`
-  from {
-    opacity: 0;
-    transform: translateY(-1000px);
-  }
-  to {
-    opacity: 1;
-    transform: translateY(0);
-  }
-`;
-
-const CenteredBox = styled.div`
-    display: flex;
-    height: 100vh;
-    width: 100%;
-    justify-content: center;
-    align-items: center;
-`;
-
-const ButtonContainer = styled.div`
-    button {
-        margin-bottom: 8px;
-        &:last-child {
-            margin-bottom: 0;
-        }
-    }
-`;
-
-const StyledCard = styled(Card)`
-    animation: ${blendIn} 1.5s;
-`;
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import StartScreen from "./StartScreen";
 
 function App() {
     return (
-        <CenteredBox>
-            <StyledCard title="Simple DnD">
-                <ButtonContainer>
-                    <Button type="primary" block>
-                        Start Game
-                    </Button>
-                    <Button type="primary" block>
-                        Create Character
-                    </Button>
-                </ButtonContainer>
-            </StyledCard>
-        </CenteredBox>
+        <Router>
+            <Route path="/" exact component={StartScreen} />
+        </Router>
     );
 }
 
